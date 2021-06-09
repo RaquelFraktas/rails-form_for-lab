010d1998fb0815390cf6498fd6cd6c492b08162c
github username <github email address>
2021-06-08 22:18:36 -0400

    commit tree 6e37fe10cb63e7f2aa4be3bffee9f313d88c3a38

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,14 +11,16 @@ class SchoolClassesController < ApplicationController
     end
     
     def show
+        @school_class = SchoolClass.find(params[:id])
     end
     
     def edit
+        @school_class = SchoolClass.find(params[:id])
     end
 
     def update
       @school_class = SchoolClass.find(params[:id])
-      @school_class.update = SchoolClass.update(school_class_params(:title, :room_number))
+      @school_class = SchoolClass.update(school_class_params(:title, :room_number))
       redirect_to school_class_path(@school_class)
     end
 
